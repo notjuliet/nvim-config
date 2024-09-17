fbc95a0a03e9d710498d5e14e3d56d5ad74235cd
notjuliet <notjuliet@riseup.net>
2024-09-17 23:50:08 +0200

    update

--- init.lua
+++ init.lua
@@ -491,8 +491,23 @@ require('lazy').setup({
 
   { 'windwp/nvim-ts-autotag', lazy = false, opts = {} },
 
-  require 'plugins.indent_line',
-  require 'plugins.autopairs',
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = {},
+  },
+
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = function()
+      require('nvim-autopairs').setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 }, {
   ui = {
