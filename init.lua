5e4c5ed2ad923d6f004bdea654346840c800414e
notjuliet <notjuliet@riseup.net>
2024-11-05 08:54:25 +0100

    update

--- init.lua
+++ init.lua
@@ -251,6 +251,14 @@ require("lazy").setup({
 				end,
 			})
 
+			if vim.g.have_nerd_font then
+				local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+				for type, icon in pairs(signs) do
+					local hl = "DiagnosticSign" .. type
+					vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+				end
+			end
+
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
