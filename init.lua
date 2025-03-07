1d10769f672cc3eaa74da82808e382ada6ffbf4f
Juliet <m@juli.ee>
2025-03-06 22:26:39 -0800

    dashboard

--- init.lua
+++ init.lua
@@ -497,7 +497,71 @@ require("lazy").setup({
 		---@type snacks.Config
 		opts = {
 			bigfile = { enabled = true },
-			dashboard = { enabled = true },
+			dashboard = {
+				enabled = true,
+				sections = {
+					{ section = "header" },
+					{
+						pane = 2,
+						padding = 7,
+					},
+					{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+					{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+					{ section = "startup" },
+					{
+						pane = 2,
+						icon = " ",
+						desc = "Browse Repo",
+						padding = 1,
+						key = "b",
+						action = function()
+							Snacks.gitbrowse()
+						end,
+					},
+					function()
+						local in_git = Snacks.git.get_root() ~= nil
+						local cmds = {
+							{
+								title = "Open Issues",
+								cmd = "gh issue list -L 3",
+								key = "i",
+								action = function()
+									vim.fn.jobstart("gh issue list --web", { detach = true })
+								end,
+								icon = " ",
+								height = 7,
+							},
+							{
+								icon = " ",
+								title = "Open PRs",
+								cmd = "gh pr list -L 3",
+								key = "P",
+								action = function()
+									vim.fn.jobstart("gh pr list --web", { detach = true })
+								end,
+								height = 7,
+							},
+							{
+								icon = " ",
+								title = "Git Status",
+								cmd = "git --no-pager diff --stat -B -M -C",
+								height = 10,
+							},
+						}
+						return vim.tbl_map(function(cmd)
+							return vim.tbl_extend("force", {
+								pane = 2,
+								section = "terminal",
+								enabled = in_git,
+								padding = 1,
+								ttl = 5 * 60,
+								indent = 3,
+							}, cmd)
+						end, cmds)
+					end,
+				},
+			},
 			explorer = { enabled = true },
 			indent = { enabled = true },
 			input = { enabled = true },
