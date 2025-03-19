63c4350adf9a771e7385ba1d1a02f50ca77c1524
Juliet <m@juli.ee>
2025-03-19 04:06:01 -0700

    modify dashboard

--- init.lua
+++ init.lua
@@ -484,16 +484,9 @@ require("lazy").setup({
 				enabled = true,
 				sections = {
 					{ section = "header" },
-					{
-						pane = 2,
-						padding = 7,
-					},
 					{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
 					{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-					{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-					{ section = "startup" },
 					{
-						pane = 2,
 						icon = " ",
 						desc = "Browse Repo",
 						padding = 1,
@@ -502,47 +495,20 @@ require("lazy").setup({
 							Snacks.gitbrowse()
 						end,
 					},
-					function()
-						local in_git = Snacks.git.get_root() ~= nil
-						local cmds = {
-							{
-								title = "Open Issues",
-								cmd = "gh issue list -L 3",
-								key = "i",
-								action = function()
-									vim.fn.jobstart("gh issue list --web", { detach = true })
-								end,
-								icon = " ",
-								height = 7,
-							},
-							{
-								icon = " ",
-								title = "Open PRs",
-								cmd = "gh pr list -L 3",
-								key = "P",
-								action = function()
-									vim.fn.jobstart("gh pr list --web", { detach = true })
-								end,
-								height = 7,
-							},
-							{
-								icon = " ",
-								title = "Git Status",
-								cmd = "git --no-pager diff --stat -B -M -C",
-								height = 10,
-							},
-						}
-						return vim.tbl_map(function(cmd)
-							return vim.tbl_extend("force", {
-								pane = 2,
-								section = "terminal",
-								enabled = in_git,
-								padding = 1,
-								ttl = 5 * 60,
-								indent = 3,
-							}, cmd)
-						end, cmds)
-					end,
+					{
+						icon = " ",
+						title = "Git Status",
+						section = "terminal",
+						enabled = function()
+							return Snacks.git.get_root() ~= nil
+						end,
+						cmd = "git status --short --branch --renames",
+						height = 5,
+						padding = 1,
+						ttl = 5 * 60,
+						indent = 3,
+					},
+					{ section = "startup" },
 				},
 			},
 			explorer = { enabled = true },
