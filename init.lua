40f6fedb2ea0c3deddb497961f89b833c05737b6
Juliet <m@juli.ee>
2025-03-30 18:18:52 +0200

    switch colorscheme

--- init.lua
+++ init.lua
@@ -345,16 +345,12 @@ require("lazy").setup({
 	},
 
 	{
-		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
+		"zenbones-theme/zenbones.nvim",
+		dependencies = "rktjmp/lush.nvim",
+		lazy = false,
+		priority = 1000,
 		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("tokyonight").setup({
-				styles = {
-					comments = { italic = false },
-				},
-			})
-			vim.cmd.colorscheme("tokyonight")
+			vim.cmd.colorscheme("zenwritten")
 		end,
 	},
 
