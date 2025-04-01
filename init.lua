dc33ab9b00c5d3e397aaa0fe0082107d3b24c80a
Juliet <m@juli.ee>
2025-04-01 12:06:39 +0200

    force truncated mode in statusline

--- init.lua
+++ init.lua
@@ -370,7 +370,7 @@ require("lazy").setup({
 
 			local statusline = require("mini.statusline")
 			local content = function()
-				local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+				local mode, mode_hl = statusline.section_mode({ trunc_width = 9999 })
 				local git = statusline.section_git({ trunc_width = 40 })
 				local diff = statusline.section_diff({ trunc_width = 75 })
 				local diagnostics = statusline.section_diagnostics({
