caad53438f3acc547def141cda676da6cef3a821
Juliet <m@juli.ee>
2025-04-02 10:09:46 +0200

    diagnostic fix

--- init.lua
+++ init.lua
@@ -42,6 +42,45 @@ vim.diagnostic.config({
 	virtual_text = true,
 })
 
+local og_virt_text
+local og_virt_line
+vim.api.nvim_create_autocmd({ "CursorMoved", "DiagnosticChanged" }, {
+	group = vim.api.nvim_create_augroup("diagnostic_only_virtlines", {}),
+	callback = function()
+		if og_virt_line == nil then
+			og_virt_line = vim.diagnostic.config().virtual_lines
+		end
+
+		-- ignore if virtual_lines.current_line is disabled
+		if not (og_virt_line and og_virt_line.current_line) then
+			if og_virt_text then
+				vim.diagnostic.config({ virtual_text = og_virt_text })
+				og_virt_text = nil
+			end
+			return
+		end
+
+		if og_virt_text == nil then
+			og_virt_text = vim.diagnostic.config().virtual_text
+		end
+
+		local lnum = vim.api.nvim_win_get_cursor(0)[1] - 1
+
+		if vim.tbl_isempty(vim.diagnostic.get(0, { lnum = lnum })) then
+			vim.diagnostic.config({ virtual_text = og_virt_text })
+		else
+			vim.diagnostic.config({ virtual_text = false })
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+	group = vim.api.nvim_create_augroup("diagnostic_redraw", {}),
+	callback = function()
+		pcall(vim.diagnostic.show)
+	end,
+})
+
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
